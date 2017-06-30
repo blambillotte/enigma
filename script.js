e18eaf23b98a5b0c8562c57cb314ee93217ea101
Ben Lambillotte <ben.lambillotte@gmail.com>
2017-06-29 23:11:11 -0700

    Added User Input for Shift, Moved Write to Document into own function

--- script.js
+++ script.js
@@ -19,22 +19,38 @@ function keyHighlight(a) {
       }
 }
 
-window.addEventListener('keydown', keyHighlight); //when someone presses a key, then call keyHighlight
+ window.addEventListener('keydown', keyHighlight); //when someone presses a key, then call keyHighlight
 
 
 function Shift(e) {
   var keyCode = e.keyCode;
     if (keyCode >= 65 && keyCode <= 90) {
 
-    var plainText = e.keyCode - 65;
-    var shift = 4;
-    var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    var cipherText = (plainText + shift) % 26;
+    var plainText = (e.keyCode - 65);
+    var shift = document.getElementById("Shift").value;
+    var shiftInput = document.getElementById("Shift");
 
+    var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+
+    var cipherText = (plainText + Number(shift)) % 26;
+
+    shiftInput.value ++;
+    writeData()
+    // keyHighlight()
+
+    //console.log(shiftInput.value);
     console.log(alphabet[plainText] + ' -> ' + alphabet[cipherText]);
-    document.getElementById("output").insertAdjacentHTML("beforeend", alphabet[cipherText]);
+    console.log('shift = ' + shift + ' plainText = ' + plainText + ' cipherText = '+ cipherText);
+
     } else {
     return;
     }
+
+    function writeData(){
+      document.getElementById("output").insertAdjacentHTML("beforeend", alphabet[cipherText]);
+      document.getElementById("outputText").insertAdjacentHTML("beforeend", alphabet[plainText]);
+    }
 }
+
 window.addEventListener('keydown', Shift);
