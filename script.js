b1da3033ced33cd72e9e654e2611738147ac240e
Ben Lambillotte <ben.lambillotte@gmail.com>
2017-07-19 11:03:24 -0700

    Changes from code review with Alex

--- script.js
+++ script.js
@@ -1,69 +1,67 @@
+(function() {
+  var ALPHABET = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var $shift = document.getElementById("Shift");
 
+  function keyHighlight(a) {
+    var keyPress = document.querySelector(`.key[data-key="${a.keyCode}"]`);
+    var keyCode = a.keyCode;
+      if (keyCode >= 65 && keyCode <= 90) {
 
-function keyHighlight(a) {
-  var keyPress = document.querySelector(`.key[data-key="${a.keyCode}"]`);
-  var keyCode = a.keyCode;
-    if (keyCode >= 65 && keyCode <= 90) {
+      keyPress.classList.add('pressed'); //this adds the pressed class
 
-    keyPress.classList.add('pressed'); //this adds the pressed class
+      function removeTransition(a) {
+        if(a.propertyName !== 'transform') return; //stop if it is not a transition
+        this.classList.remove('pressed'); //removes the class of playing once the transion is done
+        }
 
-    function removeTransition(a) {
-      if(a.propertyName !== 'transform') return; //stop if it is not a transition
-      this.classList.remove('pressed'); //removes the class of playing once the transion is done
-      }
+        var keys = document.querySelectorAll('.key');
+        keys.forEach(function(key){
+          key.addEventListener('transitionend', removeTransition);
+        }); //listens to see when the CSS transition happens, then removes the transitioned state.
 
-      var keys = document.querySelectorAll('.key');
-      keys.forEach(function(key){
-        key.addEventListener('transitionend', removeTransition);
-      }); //listens to see when the CSS transition happens, then removes the transitioned state.
+        } else {
+          return;
+        }
+  }
 
-      } else {
-        return;
-      }
-}
+   window.addEventListener('keydown', keyHighlight); //when someone presses a key, then call keyHighlight
 
- window.addEventListener('keydown', keyHighlight); //when someone presses a key, then call keyHighlight
+  function incrementShiftIndex() {
+    $shift.value ++;
+  }
 
+  function displayLetters(cipherIndex, alphaIndex){
+    document.getElementById("outputText").insertAdjacentHTML("beforeend", ALPHABET[cipherIndex]);
+    document.getElementById("inputText").insertAdjacentHTML("beforeend", ALPHABET[alphaIndex]);
+  }
 
-function Shift(plainText) {
+  function shift(alphaIndex) {
 
-  var shift = Number(document.getElementById("Shift").value);
-  var shiftInput = document.getElementById("Shift");
+    var shiftIndex = Number($shift.value);
+    var cipherIndex = (alphaIndex + shiftIndex) % 26;
 
-  var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    incrementShiftIndex();
+    displayLetters(cipherIndex, alphaIndex);
 
-  var cipherText = (plainText + shift) % 26;
+    //console.log($shift.value);
+    console.log(ALPHABET[alphaIndex] + ' → ' + ALPHABET[cipherIndex]);
 
-  shiftInput.value ++;
-  writeData()
-
-  //console.log(shiftInput.value);
-  console.log(alphabet[plainText] + ' → ' + alphabet[cipherText]);
-
-
-    function writeData(){
-      document.getElementById("outputText").insertAdjacentHTML("beforeend", alphabet[cipherText]);
-      document.getElementById("inputText").insertAdjacentHTML("beforeend", alphabet[plainText]);
+    var DEBUG = {
+      "Shift": shiftIndex,
+      "Plaintext": alphaIndex,
+      "cipherText": cipherIndex,
     }
-
-  var DEBUG = {
-    "Shift": shift,
-    "Plaintext": plainText,
-    "cipherText": cipherText,
+    console.log(DEBUG);
   }
-  console.log(DEBUG);
-}
 
-
-function alphaCode(event) {
-  var keyCode = event.keyCode
-
-  if (keyCode >= 65 && keyCode <= 90) {
-    var plainText = keyCode - 65;
-    Shift(plainText);
-  } else {
-    return;
+  function getAlphaIndex(rawKeyCode) {
+    return (rawKeyCode >= 65 && rawKeyCode <= 90) ? rawKeyCode - 65 : null;
   }
-}
 
-window.addEventListener('keydown', alphaCode);
+  function handleKeydown(event) {
+    var alphaIndex = getAlphaIndex(event.keyCode);
+    if (alphaIndex != null) return shift(alphaIndex);
+  }
+
+  window.addEventListener('keydown', handleKeydown);
+})();
